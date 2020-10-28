768205d959014d6c966d51bc66e944d044413e88
isuharev <ipupkin39@gmail.com>
2020-10-28 12:25:42 +0300

    Добавлена система аутентификации. Теперь только авторизованные пользователи могут просматривать или редактировать журнал. Если же пользователь не выполнил вход в систему, то его выбросит на страницу, где будет предложено выполнить вход.

--- journal/urls.py
+++ journal/urls.py
@@ -1,4 +1,4 @@
-from django.urls import path
+from django.urls import path, include
 from .views import JournalView, JournalNewEntry
 from django.contrib import admin
 
@@ -6,7 +6,8 @@ from django.contrib import admin
 # тут прописаны URL маршруты. Благодаря им прога знает, для каких адресов какие вьюшки задействовать.
 urlpatterns = [
     path('', JournalView.as_view(), name='home'),
-    path('entry/new/', JournalNewEntry.as_view(), name='new_entry')
+    path('entry/new/', JournalNewEntry.as_view(), name='new_entry'),
+    path('accounts/', include('django.contrib.auth.urls'))
 ]
 admin.site.site_header = "Административная панель"
 admin.site.index_title = "Управление"
