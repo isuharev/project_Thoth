d4098b63cdb21b4aef7270f18ba404c87a0d63ac
isuxarev <ipupkin39@gmail.com>
2020-12-02 22:09:52 +0300

    Теперь в панели админа добавлена возможность импорта\экспорта таблицы. Пока, правда, работает на "отлично" только экспорт. При импорте же возникает проблема: в полях даты и времени он почему-то вне зависимости от содержимого файла подставляет текущее дату и время. Буду разбираться.

--- journal/admin.py
+++ journal/admin.py
@@ -1,6 +1,17 @@
 from django.contrib import admin
+from import_export import resources
+from import_export.admin import ImportExportModelAdmin
 from .models import Entry
 
-# Register your models here.
 
-admin.site.register(Entry)
+class EntryResource(resources.ModelResource):
+
+    class Meta:
+        model = Entry
+
+
+class ImportExportAdmin(ImportExportModelAdmin):
+    resource_class = EntryResource
+
+
+admin.site.register(Entry, ImportExportAdmin)
--- journal_db/settings.py
+++ journal_db/settings.py
@@ -29,6 +29,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'journal',
+    'import_export',
 ]
 
 MIDDLEWARE = [
