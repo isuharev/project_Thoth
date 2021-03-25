d643a934171aa07c41da17229fb4b05283fe59ee
isuharev <ipupkin39@gmail.com>
2021-03-25 12:47:18 +0300

    test commit 2

--- contracts/views.py
+++ contracts/views.py
@@ -20,12 +20,14 @@ class AddNewContactView(CreateView):
     template_name = 'new_contract.html'
 
     def form_valid(self, form):
+        
         obj = form.save(commit=False)
         obj.author = self.request.user
         obj.departament = "Не указан"
 
         # Если указаны все данные, а номер договора не содержит символа "/", то формируем полное имя
         if obj.contract_index:
+
             if "/" not in str(obj.number).strip():
                 
                 obj.contract_full_name = str(obj.departament_code)\
@@ -33,6 +35,7 @@ class AddNewContactView(CreateView):
                                         + "-" + str(obj.number)\
                                         + "/" + str(datetime.datetime.now().year)\
                                         + " от " + str(obj.contract_date)
+
             else:
 
                 # Если в номере есть символ "/", значит у него уже есть год заключения и текущий год добавлять не нужно
@@ -50,10 +53,12 @@ class AddNewContactView(CreateView):
                                         + " от " + str(obj.contract_date)
                 
             else:
+
                 # Если в номере есть символ "/", значит у него уже есть год заключения и текущий год добавлять не нужно
                 obj.contract_full_name = str(obj.departament_code)\
                                         + "-" + str(obj.number)\
                                         + " от " + str(obj.contract_date)
+
         obj.save()
         return super(AddNewContactView, self).form_valid(form)
 
